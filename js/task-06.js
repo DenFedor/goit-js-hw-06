d4185e04aff094ffc4a0554be72fa963251b7929
DenFedor <fedorenko.wo@gmail.com>
2022-10-12 17:26:23 +0300

    T3,T6 bugfix

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,10 @@
 const inputSelector = document.querySelector(`#validation-input`);
 inputSelector.addEventListener(`blur`, (event) => {
-    const symbolAmount = event.currentTarget.value;
-    inputSelector.classList.remove(`valid`, `invalid`);
-    if (symbolAmount.length !== 0) {
-        symbolAmount.length > inputSelector.dataset.length ? inputSelector.classList.add(`invalid`) : inputSelector.classList.add(`valid`);
-    }
-})
+  const symbolAmount = event.currentTarget.value;
+  inputSelector.classList.remove(`valid`, `invalid`);
+  if (symbolAmount.length !== 0) {
+    symbolAmount.length == inputSelector.dataset.length
+      ? inputSelector.classList.add(`valid`)
+      : inputSelector.classList.add(`invalid`);
+  }
+});
